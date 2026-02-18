ca3ff3458022ce7e9fcb550680cc1fe07ca55367
JUAN ALONSO ROQUE GONZALEZ <Alonso.Roque@alumno.udg.mx>
2026-02-18 16:43:51 -0600

    Fixe Supabase

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,4 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jblibgbxfqbyinkvlgsk.supabase.co';
 const supabaseKey = 'sb_publishable_YO1Ch0EZuCOAl_DKHlPExw_SAe9ERHj';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabase = createClient(
+  import.meta.env.PUBLIC_SUPABASE_URL!,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY!
+);
